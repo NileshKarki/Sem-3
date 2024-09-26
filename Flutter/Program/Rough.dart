ddf4bd1ca3f80629e559ec5725b35047ffdde99f
Nilesh Karki <nileshkarki835@gmail.com>
2024-09-26 10:41:10 +0530

    Add or Remove files

---
+++ Flutter/Program/Rough.dart
@@ -0,0 +1,12 @@
+void main () { 
+  // define a variable which holds a numeric value 
+  var age = 16; 
+ 
+  // if statement to check the given condition  
+  if (age > 18) { 
+    print("You are eligible for voting"); 
+  }; 
+ 
+  print("You are not eligible for voting"); 
+} 
+ 
